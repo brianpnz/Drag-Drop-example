28bcf165ca78f45696c420c6b575fd015a3b10dd
Brian Payne <brianp.nz@gmail.com>
2018-10-21 17:10:57 +1300

    Initial commit

---
+++ Assets/Slot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IDropHandler {
+
+	public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 0){
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+    }
+
+    // Teh recieving object
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!item)
+        {
+            //  Get the static DragHandler object and the item, then set as a child of this transform
+            DragHandler.itemBeingDragged.transform.SetParent(transform);
+        }
+
+    }
+}
